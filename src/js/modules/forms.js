ba68c8bdf686f7729cc27f512b342bf8281f85f5
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-09-01 09:48:34 +0300

    add db

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,5 @@
 // import checkNumImputs from './checkNumInputs';
+import {postData} from '../services/requests';
 
 const forms = () =>{
     const form = document.querySelectorAll('form'),
@@ -20,14 +21,6 @@ const forms = () =>{
         question: 'assets/question.php'
     };
 
-    const postData = async (url, data) =>{
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
-    };
 
     const clearInputs = () => {
         inputs.forEach(item => {
