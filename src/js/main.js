e7625e07b583a81df4c27b7f9c24654a442aff96
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-09-04 12:19:40 +0300

    add accordion

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import hoseCalc from './modules/hoseCalc';
+import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -20,4 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     hoseCalc('#nutSize', '.hoseLenght', '.hose-price');
+    filter();
+    pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 });
